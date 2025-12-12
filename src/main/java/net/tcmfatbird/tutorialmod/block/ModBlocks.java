3b1737da38deae5977e95ad658da42ebded9e3bf
TheyCallMeFatbird <FatbirdYT@outlook.com>
2025-12-11 19:06:37 -0600

    new block that can easily give diamondz LOLL

--- src/main/java/net/tcmfatbird/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/tcmfatbird/tutorialmod/block/ModBlocks.java
@@ -13,6 +13,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.tcmfatbird.tutorialmod.block.custom.MagicBlock;
 
 public class ModBlocks {
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
@@ -29,6 +30,9 @@ public class ModBlocks {
     public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
             new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
                     AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+    public static final Block MAGIC_BLOCK = registerBlock("magic_block",
+            new MagicBlock(AbstractBlock.Settings.create().strength(1f).requiresTool()));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
